76adacaac31d57dfbcaa92dfb8b1cfbc453bf88d
khacquan <48463737+khacquan@users.noreply.github.com>
2021-07-01 16:55:18 +0700

    Add files via upload

---
+++ TieuLuanHeThongNhung/FileMaNguon/cau1.1.c
@@ -0,0 +1,22 @@
+#include <REGX51.H>
+void delay(int n)
+{
+   int i;
+   for(i=0;i<=n;i++);
+}
+void hienthi(int chuc,donvi)
+{
+   P3_1 = 1;
+   P2 = chuc;
+   P3_1 = 0;
+   P3_0 = 1;
+   P2 = donvi;
+   P3_0 = 0;
+}
+void main()
+{
+   while(1)
+   {
+	  hienthi(6,8);
+   }
+}
